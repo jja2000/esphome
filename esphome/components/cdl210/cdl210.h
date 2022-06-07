0ab133ee66bc714867517309831226e3168efe6b
Samuel Sieb <samuel-github@sieb.net>
2022-06-07 14:29:52 -0700

    Update cdl210.h

--- esphome/components/cdl210/cdl210.h
+++ esphome/components/cdl210/cdl210.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include "esphome/core/component.h"
-#include "esphome/core/automation.h"
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/components/uart/uart.h"
 
@@ -10,7 +9,6 @@ namespace cdl210 {
 
 class CDL210Component : public PollingComponent, public uart::UARTDevice {
  public:
-  void setup() override;
   void update() override;
   void loop() override;
   void dump_config() override;
@@ -20,10 +18,7 @@ class CDL210Component : public PollingComponent, public uart::UARTDevice {
   void set_humidity_sensor(sensor::Sensor *humidity_sensor) { humidity_sensor_ = humidity_sensor; }
 
  protected:
-  bool cdl210_write_command_(const uint8_t *command, uint8_t *response);
   std::vector<uint8_t> rx_message_;
-  std::string data;
-  void handle_char_(uint8_t c, std::string *s);
 
   sensor::Sensor *temperature_sensor_{nullptr};
   sensor::Sensor *co2_sensor_{nullptr};
